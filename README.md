67d9af035f77c7f23d433069e6a193800da4c9b1
Nadim Edde Gomez <nadim@eddegomez.org>
2025-08-26 11:36:00 +0100

    feat(all): init repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nomad
+
+Nomad is a colorscheme based on [Spacecamp](https://github.com/jaredgorski/spacecamp.vim)
+
+## Design
+
+The theme simply uses `vim.api.nvim_set_hl` to set highlight colors and does not aim to provide any customisability.
+
+## Screenshot
---
+++ lua/nomad/colors.lua
@@ -0,0 +1,30 @@
+local colors = {}
+colors.dark = {
+	void = "#121212",
+	base00 = "#1C1C1C",
+	base01 = "#262626",
+	base02 = "#303030",
+	base03 = "#444444",
+	base04 = "#D0D0D0",
+	base05 = "#EEEEEE",
+	base06 = "#f2f4f8",
+	alien = "#CDFD00",
+	blue = "#91AADF",
+	purple = "#DD68EC",
+	pink = "#FFAFF3",
+	green = "#3FC56B",
+	yellow = "#F0D50C",
+	orange = "#F66C00",
+	red = "#DF3232",
+	grey = "#6B6B6B",
+
+	none = "NONE",
+	Selection = "#264F78",
+
+	DiffRedDark = "#4B1818",
+	DiffRedLight = "#6F1313",
+	DiffGreenDark = "#373D29",
+	DiffGreenLight = "#4B5632",
+}
+
+return colors
